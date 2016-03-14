3aa5ef6374b7fb22252dbd812c647cdbd9397ed5
Boris Kourtoukov <boriskourt@gmail.com>
2016-03-14 15:15:12 -0400

    Upgrade jmdns to latest, add mvn rep for jmdns, and add jmdns dep for slf4j-simple

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "https://github.com/cassiel/clojure-zeroconf"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[javax.jmdns/jmdns "3.4.1"]
-                 [org.clojure/clojure "1.5.1"]]
-  :plugins [[lein-marginalia "0.7.1"]])
+  :dependencies [[org.slf4j/slf4j-api "1.7.18"]
+                 [org.slf4j/slf4j-simple "1.7.18"]
+                 [org.jmdns/jmdns "3.5.0"]
+                 [org.clojure/clojure "1.6.0"]]
+  :repositories {"bintray" "https://dl.bintray.com/jmdns/mvn/"}
+  :plugins [[lein-marginalia "0.8.0"]])
